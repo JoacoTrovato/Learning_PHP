40eb4abc15034ba41f871d1061dccdc2f65434d8
JoacoTrovato <joacotrovato@gmail.com>
2022-05-13 10:13:08 -0300

    Finish

--- 17-require.php
+++ 17-require.php
@@ -1,8 +1,10 @@
 <?php include 'includes/header.php';
 
+// require se recomienda para incluir funciones que van a ser claves en el proyecto
+require 'funciones.php';
 
+// Acá llamamos a la función del archivo funciones.php
+iniciarApp();
 
-
-
-
+// include es recomendado para incluir los templates al proyecto
 include 'includes/footer.php';
--- 18.json.php
+++ 18.json.php
@@ -1,8 +1,36 @@
 <?php include 'includes/header.php';
 
+$productos = [
+    [
+        'nombre' => 'Tablet',
+        'precio' => 200,
+        'disponible' => true
+    ],
+    [
+        'nombre' => 'Televisión 24"',
+        'precio' => 300,
+        'disponible' => true
+    ],
+    [
+        'nombre' => 'Monitor Curvo',
+        'precio' => 400,
+        'disponible' => false
+    ]
+];
 
+echo "<pre>";
+var_dump($productos);
 
+// Para poder convertir el string a JSON y que JavaScript pueda leerlo
 
+$json = json_encode($productos, JSON_UNESCAPED_UNICODE);
+// json_encode convierte un array a string
 
+$json_array = json_decode($json);
+// json_decode convierte un string a un array
+
+var_dump($json);
+var_dump($json_array);
+echo "</pre>";
 
 include 'includes/footer.php';
